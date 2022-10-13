63e80d1f69d44a0a5f852d48cf97355a9fe77e93
Euclides do Rosário <euclidesdry@gmail.com>
2022-10-13 23:59:35 +0100

    test: ✅ updating jest configs

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,12 @@
 export default {
+  testEnvironment: "jsdom",
   transform: {
     "^.+\\.tsx?$": "ts-jest",
   },
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/test/__mocks__/fileMock.js",
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+  },
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
 };
